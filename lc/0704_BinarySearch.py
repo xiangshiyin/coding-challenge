732ff5d7c6033d35a95f5884447dd080f9fe18d6
Xiangshi Yin <piupiuyin@gmail.com>
2021-11-07 14:11:17 -0500

    Create 0704_BinarySearch.py
    binary search

---
+++ lc/0704_BinarySearch.py
@@ -0,0 +1,17 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if len(nums) == 0:
+            return -1
+        
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if target == nums[m]:
+                return m
+            elif target > nums[m]:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
+        
