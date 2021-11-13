33cc0e365323f1983534c3e97bfa698c2d130937
Xiangshi Yin <piupiuyin@gmail.com>
2021-11-12 23:24:03 -0500

    Update 0876_MiddleOfTheLinkedList.py

--- lc/0876_MiddleOfTheLinkedList.py
+++ lc/0876_MiddleOfTheLinkedList.py
@@ -3,20 +3,32 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# class Solution:
+#     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         # 1. get the length of the linked list
+#         h = ListNode(next=head)
+#         n = 0
+#         while h.next:
+#             n += 1
+#             h = h.next
+        
+#         # 2. find the middle node
+#         counter = 0
+#         h = ListNode(next=head)
+#         while counter <= n // 2:
+#             h = h.next
+#             counter += 1
+#         return h
+
+# 2nd solution as of 11/12/2021
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # 1. get the length of the linked list
-        h = ListNode(next=head)
-        n = 0
-        while h.next:
-            n += 1
-            h = h.next
-        
-        # 2. find the middle node
-        counter = 0
-        h = ListNode(next=head)
-        while counter <= n // 2:
-            h = h.next
-            counter += 1
-        return h
+        '''
+        fast and slow pointers
+        '''
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
         
