b106d7545cec7027da14a85cd3cb3517800e1071
Xiangshi Yin <piupiuyin@gmail.com>
2021-11-12 23:37:28 -0500

    Update 0019_RemoveNthNodeFromEndOfList.py

--- lc/0019_RemoveNthNodeFromEndOfList.py
+++ lc/0019_RemoveNthNodeFromEndOfList.py
@@ -3,30 +3,54 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# class Solution:
+#     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+#         stack = []
+#         node = head
+#         while node:
+#             stack.append(node)
+#             node = node.next
+        
+#         ## pop out
+#         counter = 0
+#         out = None
+#         while counter < n - 1:
+#             out = stack.pop()
+#             counter += 1
+        
+#         node2del = stack.pop()
+        
+#         if not stack:
+#             if out == None:
+#                 head = None
+#             else:
+#                 head = out
+#         else:
+#             stack[-1].next = out
+        
+#         return head
+
+
 class Solution:
-    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        stack = []
-        node = head
-        while node:
-            stack.append(node)
-            node = node.next
-        
-        ## pop out
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        '''
+        fast and slow pointers
+        '''
+        slow = fast = head
+        # move the fast pointer first
         counter = 0
-        out = None
-        while counter < n - 1:
-            out = stack.pop()
+        while counter < n:
             counter += 1
+            fast = fast.next
+
+        # move both the slow and fast pointers
+        dh = prev = ListNode(next=head)
+        while fast:
+            prev = slow
+            slow = slow.next
+            fast = fast.next
+        prev.next = slow.next
         
-        node2del = stack.pop()
-        
-        if not stack:
-            if out == None:
-                head = None
-            else:
-                head = out
-        else:
-            stack[-1].next = out
-        
-        return head
+        return dh.next
+    
     
