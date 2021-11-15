5b4599f87c6444e6376716f5313102e5c0824450
Xiangshi Yin <piupiuyin@gmail.com>
2021-11-15 13:40:46 -0500

    Create 0617_MergeTwoBinaryTrees.py

---
+++ lc/0617_MergeTwoBinaryTrees.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        '''
+        with stack
+        '''
+        # exceptions
+        if not root1:
+            return root2
+        if not root2:
+            return root1
+        
+        stack = []
+        stack.append([root1, root2])
+        while stack:
+            l, r = stack.pop()
+            # update l node val
+            l.val += r.val
+            # append children
+            if l.left and r.left:
+                stack.append([l.left, r.left])
+            elif not l.left:
+                l.left = r.left
+            
+            if l.right and r.right:
+                stack.append([l.right, r.right])
+            elif not l.right:
+                l.right = r.right
+
+        return root1
+    
+    
+    
