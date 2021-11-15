2cfefb9308025ed340f6238aa096756da6fd4bc8
Xiangshi Yin <piupiuyin@gmail.com>
2021-11-14 22:43:55 -0500

    permutation first, lookup later

--- lc/0567_PermutationInString.py
+++ lc/0567_PermutationInString.py
@@ -31,5 +31,35 @@ class Solution:
             if mp2 == mp1:
                 return True
             
+# class Solution:
+#     def checkInclusion(self, s1: str, s2: str) -> bool:
+#         '''
+#         time limit exceeded
+#         '''
+#         # exceptions
+#         if len(s2) < len(s1):
+#             return False
         
+#         def swap(s, first, output):
+#             n = len(s)
+#             if first == n:
+#                 output.add(''.join(s[:]))
+#             for i in range(first, n):
+#                 if s[i] != s[first]:
+#                     s[i],s[first] = s[first],s[i]
+#                 swap(s, first+1, output)
+#                 if s[i] != s[first]:
+#                     s[i],s[first] = s[first],s[i]
+#         # find all permutations of s1
+#         perm1 = set()
+#         swap(list(s1), 0, perm1)
+        
+#         # traverse s2
+#         for i in range(len(s2) - len(s1) + 1):
+#             if s2[i:(i+len(s1))] in perm1:
+#                 return True
+            
+#         return False
+    
+    
         
