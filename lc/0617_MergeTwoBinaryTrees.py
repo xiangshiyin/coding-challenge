4782dbc007b485e28161bac1275b7aca031ccdc9
Xiangshi Yin <piupiuyin@gmail.com>
2021-11-15 13:45:51 -0500

    Update 0617_MergeTwoBinaryTrees.py

--- lc/0617_MergeTwoBinaryTrees.py
+++ lc/0617_MergeTwoBinaryTrees.py
@@ -33,6 +33,23 @@ class Solution:
                 l.right = r.right
 
         return root1
+
+# as of 11/15/2021, recursion, preorder traversal
+# class Solution:
+#     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+#         '''
+#         pre-order traversal
+#         '''
+#         if not root1:
+#             return root2
+#         if not root2:
+#             return root1
+        
+#         root1.val += root2.val
+#         root1.left = self.mergeTrees(root1.left, root2.left)
+#         root1.right = self.mergeTrees(root1.right, root2.right)
+        
+#         return root1
     
     
     
