795202e3c92ef48a578fe88135ee226b6e664d27
Xiangshi Yin <piupiuyin@gmail.com>
2021-11-16 16:27:19 -0500

    Update 0784_LetterCasePermutation.py

--- lc/0784_LetterCasePermutation.py
+++ lc/0784_LetterCasePermutation.py
@@ -7,4 +7,32 @@ class Solution:
             else:
                 result = [r + char for r in result]
         return result
+
+# solution as of 11/16/2021
+# class Solution:
+#     def letterCasePermutation(self, s: str) -> List[str]:
+#         def helper(s, target, output):
+#             n = len(s)
+#             if target == n:
+#                 output.append(''.join(s))
+#             else:
+#                 if s[target].isalpha():
+#                     for candidate in (s[target].lower(), s[target].upper()):
+#                         cache = s[target]
+#                         if candidate != cache:
+#                             s[target] = candidate
+
+#                         helper(s, target+1, output)
+
+#                         if candidate != cache:
+#                             s[target] = cache
+#                 else:
+#                     helper(s, target+1, output)
+        
+#         output = []
+#         s2 = list(s)
+#         helper(s2, 0, output)
+        
+#         return output
+    
     
