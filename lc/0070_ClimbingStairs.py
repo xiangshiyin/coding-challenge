30dacdba6fa090a9db9f8ab38ef618e0ba49cba2
Xiangshi Yin <piupiuyin@gmail.com>
2021-11-17 20:26:31 -0500

    Create 0070_ClimbingStairs.py

---
+++ lc/0070_ClimbingStairs.py
@@ -0,0 +1,22 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        '''
+        it all depends on the size of the last step achieving (n - 1)
+        climbStairs(n) = climbStairs(n-1, ends with 2 step) + climbStairs(n-1, ends with 1 step) * 2
+        
+        and:
+        climbStairs(n, ends with 2 step) = climbStairs(n-1, ends with 1 step)
+        climbStairs(n, ends with 1 step) = climbStairs(n-1, ends with 1 step) + climbStairs(n-1, ends with 2 step)
+        '''
+        ways = [1, 0] # [ways end with 1 step, ways end with 2 step]
+        if n == 1:
+            return ways[0] + ways[1]
+        else:
+            for i in range(2, n + 1):
+                old_1step = ways[0]
+                ways[0] = ways[0] + ways[1]
+                ways[1] = old_1step
+                
+            return ways[0] + ways[1]
+        
+        
