01fdd67bc89300a65dcd0ff1b5ae2d2b7c58174a
Xiangshi Yin <piupiuyin@gmail.com>
2021-11-26 13:48:29 -0500

    Update 0283_MoveZeros.py

--- lc/0283_MoveZeros.py
+++ lc/0283_MoveZeros.py
@@ -56,6 +56,26 @@ class Solution:
                     nums[i] = 0
         return nums
             
+# # solution as of 11/26/2021
+# class Solution:
+#     def moveZeroes(self, nums: List[int]) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+#         n = len(nums)
+#         if n == 1:
+#             return nums
+        
+#         counter = 0
+#         for i in range(n):
+#             if nums[i] == 0:
+#                 counter += 1
+#             elif counter > 0:
+#                 nums[i-counter] = nums[i]
+#                 nums[i] = 0
+        
+#         return nums
+        
         
         
 def test(nums):
