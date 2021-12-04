a75a644b96dec1b6c7146b952ca4333263f0a461
Xiangshi Yin <piupiuyin@gmail.com>
2021-12-03 22:34:00 -0500

    Update 0994_RottingOranges.py

--- lc/0994_RottingOranges.py
+++ lc/0994_RottingOranges.py
@@ -40,7 +40,46 @@ class Solution:
             return -1
         
                 
+# solution as of 12/3/2021
+# class Solution:
+#     def orangesRotting(self, grid: List[List[int]]) -> int:
+#         m = len(grid)
+#         n = len(grid[0])
+        
+#         from collections import deque
+#         # find all rotten oranges and mark all fresh oranges
+#         fresh = 0
+#         q = deque()
+#         for r in range(m):
+#             for c in range(n):
+#                 if grid[r][c] == 1:
+#                     fresh += 1
+#                 elif grid[r][c] == 2:
+#                     q.append([r,c])
+#         if not fresh:
+#             return 0
+        
+#         drcs = [[-1,0],[1,0],[0,-1],[0,1]]
+#         step = -1
+#         while q:
+#             l = len(q) # record the length of the queue
+#             print(q)
+#             for i in range(l):
+#                 sr, sc = q.popleft()
+#                 for dr,dc in drcs:
+#                     if 0<=sr+dr<m and 0<=sc+dc<n and grid[sr+dr][sc+dc]==1:
+#                         grid[sr+dr][sc+dc] = 2
+#                         fresh -= 1
+#                         q.append([sr+dr, sc+dc])
                         
+#             step += 1
+        
+#         if not fresh:
+#             return step
+#         else:
+#             return -1
+        
+        
                         
                         
                         
