5b4943db3207bc00c92855ceba5dd2f103c3ea4f
Xiangshi Yin <piupiuyin@gmail.com>
2022-08-25 13:48:53 -0400

    2 more

---
+++ lc-2022/1.two-sum.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        saw = {}
+        for idx, num in enumerate(nums):
+            if target - num in saw:
+                return [saw[target - num], idx]
+            saw[num] = idx 
+
+        
+# @lc code=end
+
---
+++ lc-2022/88.merge-sorted-array.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=88 lang=python3
+#
+# [88] Merge Sorted Array
+#
+
+# @lc code=start
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        p = m + n - 1
+        lp = m - 1
+        rp = n - 1
+        while rp >= 0:
+            if lp < 0 or nums2[rp] >= nums1[lp]:
+                nums1[p] = nums2[rp]
+                rp -= 1
+            else:
+                nums1[p] = nums1[lp]
+                lp -= 1
+            p -= 1
+        
+
+
+
+        
+# @lc code=end
+
