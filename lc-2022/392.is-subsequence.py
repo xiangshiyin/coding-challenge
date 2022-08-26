2f855989632dd85bec97f64731b3eeee7452b355
Xiangshi Yin <piupiuyin@gmail.com>
2022-08-26 13:56:52 -0400

    optimized 392

--- lc-2022/392.is-subsequence.py
+++ lc-2022/392.is-subsequence.py
@@ -7,19 +7,17 @@
 # @lc code=start
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        visited = [0] * len(s)
         ps = 0
         pt = 0
         while pt < len(t) and ps < len(s):
             while pt < len(t) and t[pt] != s[ps]:
                 pt += 1
             if pt < len(t):
-                visited[ps] = 1
                 ps += 1
                 pt += 1
             else:
                 return False
-                
+
         if ps == len(s):
             return True
         else:
