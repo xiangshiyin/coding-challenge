51fe6c9fe99dd9081c1ed08c105956edf8ad0a0c
Xiangshi Yin <piupiuyin@gmail.com>
2022-08-27 20:22:54 -0400

    1 more

---
+++ lc-2022/566.reshape-the-matrix.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=566 lang=python3
+#
+# [566] Reshape the Matrix
+#
+
+# @lc code=start
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        # check if reshape is possible
+        m = len(mat)
+        n = len(mat[0])
+        if m * n != r * c:
+            return mat
+        # reshape
+        output = [[0] * c for i in range(r)] # placeholder
+        for i in range(m):
+            for j in range(n):
+                idx = n * i + j
+                output[idx//c][idx%c] = mat[i][j]
+        return output
+
+
+# @lc code=end
+
