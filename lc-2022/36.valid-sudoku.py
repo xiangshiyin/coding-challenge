ad8c87c4cf25123bfc7777011fa2dc5b17ed2ea0
Xiangshi Yin <piupiuyin@gmail.com>
2022-08-28 18:04:20 -0400

    2 more

---
+++ lc-2022/36.valid-sudoku.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=36 lang=python3
+#
+# [36] Valid Sudoku
+#
+
+# @lc code=start
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        from collections import defaultdict
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        subs = defaultdict(set)
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] not in rows[i] and board[i][j] not in cols[j] and board[i][j] not in subs[3 * (i//3) + j//3]:
+                    if board[i][j] != '.':
+                        rows[i].add(board[i][j])
+                        cols[j].add(board[i][j])
+                        subs[3 * (i//3) + j//3].add(board[i][j])
+                else:
+                    return False
+        return True
+
+
+        
+# @lc code=end
+
---
+++ lc-2022/74.search-a-2-d-matrix.py
@@ -0,0 +1,44 @@
+#
+# @lc app=leetcode id=74 lang=python3
+#
+# [74] Search a 2D Matrix
+#
+
+# @lc code=start
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
+        # binary search across rows
+        u = 0
+        d = m - 1
+        while u <= d:
+            mid = (u + d) // 2
+            if matrix[mid][0] == target:
+                return True
+            if matrix[mid][0] > target:
+                if mid == 0:
+                    return False
+                d = mid - 1
+            else:
+                if mid + 1 < m and matrix[mid + 1][0] > target:
+                    break
+                else:
+                    u = mid + 1
+        
+        # binary search across cols
+        l = 0 
+        r = n - 1
+        while l <= r:
+            mid2 = (l + r) // 2
+            if matrix[mid][mid2] == target:
+                return True
+            if matrix[mid][mid2] > target:
+                r = mid2 - 1
+            else:
+                l = mid2 + 1
+        return False
+        
+        
+# @lc code=end
+
