1d55bad8dd9cad7095e5ec38890e058f3c3f7890
Xiangshi Yin <piupiuyin@gmail.com>
2022-08-30 10:15:21 -0400

    bfs level tree node

---
+++ lc-2022/102.binary-tree-level-order-traversal.py
@@ -0,0 +1,41 @@
+#
+# @lc app=leetcode id=102 lang=python3
+#
+# [102] Binary Tree Level Order Traversal
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        output = []
+        # exception
+        if not root:
+            return output
+
+        from collections import deque
+        q = deque()
+        level = 0
+        q.append([root, level])
+        while q:
+            cur = []
+            while q and q[0][1] == level:
+                node, _ = q.popleft() # pop
+                cur.append(node.val)
+                if node.left:
+                    q.append([node.left, level + 1])
+                if node.right:
+                    q.append([node.right, level + 1])
+            level += 1
+            output.append(cur)
+
+        return output
+
+        
+# @lc code=end
+
