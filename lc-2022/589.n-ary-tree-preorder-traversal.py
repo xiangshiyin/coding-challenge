84dffe0967cc015a3eade38607fc3d9ae2f8bc1f
Xiangshi Yin <piupiuyin@gmail.com>
2022-08-30 00:11:16 -0400

    1 more

---
+++ lc-2022/589.n-ary-tree-preorder-traversal.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=589 lang=python3
+#
+# [589] N-ary Tree Preorder Traversal
+#
+
+# @lc code=start
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        output = []
+        if not root:
+            return []
+        else:
+            output.append(root.val)
+            for child in root.children:
+                output.extend(self.preorder(child))
+            return output
+
+
+# @lc code=end
+
