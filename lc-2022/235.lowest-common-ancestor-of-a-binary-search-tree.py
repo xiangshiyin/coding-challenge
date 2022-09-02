4c560f82371c5128c2eb5896044afc0915d9d0b5
Xiangshi Yin <piupiuyin@gmail.com>
2022-09-02 10:46:52 -0400

    lowest common ances

---
+++ lc-2022/235.lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,48 @@
+#
+# @lc app=leetcode id=235 lang=python3
+#
+# [235] Lowest Common Ancestor of a Binary Search Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(
+        self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
+    ) -> "TreeNode":
+        # find p
+        path_p = set()
+        node = root
+        while node and node != p:
+            path_p.add(node)
+            if node.val > p.val:
+                node = node.left
+            else:
+                node = node.right
+        path_p.add(node)
+
+        # find q
+        node = root
+        output = None
+        while node != q:
+            if node in path_p:
+                output = node
+                print(output.val)
+            if node.val > q.val:
+                node = node.left
+            else:
+                node = node.right
+        if node in path_p:
+            output = q
+        return output
+
+
+# @lc code=end
+
