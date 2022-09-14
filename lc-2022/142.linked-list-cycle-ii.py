c2b01b387d8d7453bc4df5982bb65f1b611a9957
Xiangshi Yin <piupiuyin@gmail.com>
2022-09-14 11:00:05 -0400

    2 more reviews

--- lc-2022/142.linked-list-cycle-ii.py
+++ lc-2022/142.linked-list-cycle-ii.py
@@ -11,36 +11,60 @@
 #         self.val = x
 #         self.next = None
 
+# class Solution:
+#     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         if not head or not head.next:
+#             return None
+
+#         fast = slow = head
+#         step = 0
+#         # find the 1st meet
+#         while slow and fast:
+#             if not slow.next or not fast.next:
+#                 return None
+#             slow = slow.next
+#             fast = fast.next.next
+#             step += 1
+#             if fast==slow:
+#                 break
+
+#         if not slow or not fast:
+#             return None
+
+#         # find the 2nd meet
+#         slow2 = head
+#         step = 0
+#         while slow and slow2 and slow2 != slow:
+#             slow = slow.next
+#             slow2 = slow2.next
+#             step += 1
+#         return slow
+
+
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
             return None
-        
         fast = slow = head
-        step = 0
-        # find the 1st meet
-        while slow and fast:
-            if not slow.next or not fast.next:
+        while fast:  # find the first meet
+            if not fast.next:
                 return None
-            slow = slow.next
-            fast = fast.next.next
-            step += 1
-            if fast==slow:
+            fast = fast.next.next  # 2 steps
+            slow = slow.next  # 1 step
+            if fast == slow:  # found the 1st meet
                 break
-                
-        if not slow or not fast:
+
+        if not fast:
             return None
 
-        # find the 2nd meet
         slow2 = head
-        step = 0
-        while slow and slow2 and slow2 != slow:
+        slow = fast
+        while slow != slow2:
             slow = slow.next
             slow2 = slow2.next
-            step += 1
+
         return slow
-        
-        
-        
+
+
 # @lc code=end
 
--- lc-2022/876.middle-of-the-linked-list.py
+++ lc-2022/876.middle-of-the-linked-list.py
@@ -10,17 +10,38 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+
+# class Solution:
+#     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         # set slow and fast pointers
+#         fast = slow = ListNode(next = head)
+#         while fast and fast.next:
+#             slow = slow.next
+#             fast = fast.next.next
+#         if fast:
+#             return slow.next
+#         else:
+#             return slow
+
+
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # set slow and fast pointers
-        fast = slow = ListNode(next = head)
-        while fast and fast.next:
+        if not head.next:
+            return head
+        if not head.next.next:
+            return head.next
+
+        slow = head.next
+        fast = head.next.next
+        while fast:
+            if not fast.next:
+                return slow
+            if not fast.next.next:
+                return slow.next
             slow = slow.next
             fast = fast.next.next
-        if fast:
-            return slow.next
-        else:
-            return slow
-        
+
+
 # @lc code=end
 
