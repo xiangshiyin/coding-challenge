6aad4b27704a8dd8e08a856ca6709defad2b2566
Xiangshi Yin <piupiuyin@gmail.com>
2022-09-14 11:18:09 -0400

    2 more reviewed

--- lc-2022/121.best-time-to-buy-and-sell-stock.py
+++ lc-2022/121.best-time-to-buy-and-sell-stock.py
@@ -5,15 +5,31 @@
 #
 
 # @lc code=start
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         profit = 0
+#         low = 10 ** 4 + 1
+#         for p in prices:
+#             profit = max(profit, p - low)
+#             low = min(low,p)
+#         return profit
+
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        """
+        find the minimum price over the days
+        """
+        low = 0
         profit = 0
-        low = 10 ** 4 + 1
-        for p in prices:
-            profit = max(profit, p - low)
-            low = min(low,p)
+        for i, p in enumerate(prices):
+            if i == 0:
+                low = p
+            else:
+                profit = max(profit, p - low)
+                low = min(low, p)
         return profit
 
-        
+
 # @lc code=end
 
--- lc-2022/409.longest-palindrome.py
+++ lc-2022/409.longest-palindrome.py
@@ -5,20 +5,32 @@
 #
 
 # @lc code=start
+# class Solution:
+#     def longestPalindrome(self, s: str) -> int:
+#         # track letter frequencies
+#         from collections import Counter
+#         freqs = Counter(s)
+#         # find all the even pairs and odd center
+#         maxL = 0
+#         for l,f in freqs.items():
+#             maxL += (f // 2) * 2
+#             if maxL % 2 == 0 and f % 2 == 1:
+#                 maxL += 1
+#         return maxL
+
+
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        # track letter frequencies
         from collections import Counter
-        freqs = Counter(s)
-        # find all the even pairs and odd center
+
+        freq = Counter(s)
         maxL = 0
-        for l,f in freqs.items():
+        for f in freq.values():
             maxL += (f // 2) * 2
             if maxL % 2 == 0 and f % 2 == 1:
                 maxL += 1
         return maxL
 
 
-        
 # @lc code=end
 
