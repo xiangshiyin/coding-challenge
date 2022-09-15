e470acb23eabad5c6f8718a3c3e3213d623525f3
Xiangshi Yin <piupiuyin@gmail.com>
2022-09-15 00:29:25 -0400

    2 more

--- lc-2022/278.first-bad-version.py
+++ lc-2022/278.first-bad-version.py
@@ -8,19 +8,35 @@
 # The isBadVersion API is already defined for you.
 # def isBadVersion(version: int) -> bool:
 
+# class Solution:
+#     def firstBadVersion(self, n: int) -> int:
+#         l = 1
+#         r = n
+#         first = n
+#         while l <= r:
+#             mid = (l + r) // 2
+#             if isBadVersion(mid):
+#                 first = mid
+#                 r = mid - 1
+#             else:
+#                 l = mid + 1
+#         return first
+
+# solution on 2022-09-15
 class Solution:
     def firstBadVersion(self, n: int) -> int:
         l = 1
         r = n
-        first = n
+        firstBad = n
         while l <= r:
             mid = (l + r) // 2
             if isBadVersion(mid):
-                first = mid
+                firstBad = min(firstBad, mid)
                 r = mid - 1
             else:
                 l = mid + 1
-        return first
-        
+        return firstBad
+
+
 # @lc code=end
 
