53ad0dbe8b25b5d837f8e19130067d35fd26b522
Xiangshi Yin <piupiuyin@gmail.com>
2022-09-17 11:44:20 -0400

    2 dfs

--- lc-2022/200.number-of-islands.py
+++ lc-2022/200.number-of-islands.py
@@ -5,33 +5,64 @@
 #
 
 # @lc code=start
+# class Solution:
+#     def numIslands(self, grid: List[List[str]]) -> int:
+#         m = len(grid)
+#         n = len(grid[0])
+#         color = "2"
+#         counter = 0
+
+#         def dfs(grid, sr, sc, color):
+#             m = len(grid)
+#             n = len(grid[0])
+#             drc = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+#             # color the target
+#             grid[sr][sc] = color
+#             # flood the neighbors
+#             for dr, dc in drc:
+#                 if (
+#                     0 <= sr + dr < m
+#                     and 0 <= sc + dc < n
+#                     and grid[sr + dr][sc + dc] == "1"
+#                 ):
+#                     dfs(grid, sr + dr, sc + dc, color)
+
+#         for r in range(m):
+#             for c in range(n):
+#                 if grid[r][c] == "1":
+#                     counter += 1
+#                     dfs(grid, r, c, color)
+
+#         return counter
+
+
+# solution on 2022-09-17
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        color = "2"
-        counter = 0
+        drc = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        visited = [[0] * n for i in range(m)]
+        newColor = "2"
 
-        def dfs(grid, sr, sc, color):
-            m = len(grid)
-            n = len(grid[0])
-            drc = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-            # color the target
-            grid[sr][sc] = color
-            # flood the neighbors
+        def dfs(grid, r, c, newColor):
+            grid[r][c] = newColor
+            visited[r][c] = 1
             for dr, dc in drc:
                 if (
-                    0 <= sr + dr < m
-                    and 0 <= sc + dc < n
-                    and grid[sr + dr][sc + dc] == "1"
+                    0 <= r + dr < m
+                    and 0 <= c + dc < n
+                    and grid[r + dr][c + dc] == "1"
+                    and visited[r + dr][c + dc] == 0
                 ):
-                    dfs(grid, sr + dr, sc + dc, color)
+                    dfs(grid, r + dr, c + dc, newColor)
 
+        counter = 0
         for r in range(m):
             for c in range(n):
                 if grid[r][c] == "1":
                     counter += 1
-                    dfs(grid, r, c, color)
+                    dfs(grid, r, c, newColor)
 
         return counter
 
--- lc-2022/733.flood-fill.py
+++ lc-2022/733.flood-fill.py
@@ -5,32 +5,59 @@
 #
 
 # @lc code=start
+# class Solution:
+#     def floodFill(
+#         self, image: List[List[int]], sr: int, sc: int, color: int
+#     ) -> List[List[int]]:
+#         m = len(image)
+#         n = len(image[0])
+#         visited = [[0] * n for r in range(m)]
+
+#         def dfs(image, sr, sc, color):
+#             m = len(image)
+#             n = len(image[0])
+#             drc = [[0, -1], [-1, 0], [0, 1], [1, 0]]
+#             # color the target
+#             old_color = image[sr][sc]
+#             image[sr][sc] = color
+#             visited[sr][sc] = 1
+
+#             # flood the neighbors
+#             for dr, dc in drc:
+#                 if (
+#                     0 <= sr + dr < m
+#                     and 0 <= sc + dc < n
+#                     and image[sr + dr][sc + dc] == old_color
+#                     and visited[sr + dr][sc + dc] == 0
+#                 ):
+#                     dfs(image, sr + dr, sc + dc, color)
+
+#         dfs(image, sr, sc, color)
+#         return image
+
+
+# solution on 2022-09-17
 class Solution:
     def floodFill(
         self, image: List[List[int]], sr: int, sc: int, color: int
     ) -> List[List[int]]:
         m = len(image)
         n = len(image[0])
-        visited = [[0] * n for r in range(m)]
+        drc = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        visited = [[0] * n for i in range(m)]
 
-        def dfs(image, sr, sc, color):
-            m = len(image)
-            n = len(image[0])
-            drc = [[0, -1], [-1, 0], [0, 1], [1, 0]]
-            # color the target
-            old_color = image[sr][sc]
-            image[sr][sc] = color
-            visited[sr][sc] = 1
-
-            # flood the neighbors
+        def dfs(image, r, c, color):
+            old_color = image[r][c]
+            image[r][c] = color
+            visited[r][c] = 1
             for dr, dc in drc:
                 if (
-                    0 <= sr + dr < m
-                    and 0 <= sc + dc < n
-                    and image[sr + dr][sc + dc] == old_color
-                    and visited[sr + dr][sc + dc] == 0
+                    0 <= r + dr < m
+                    and 0 <= c + dc < n
+                    and image[r + dr][c + dc] == old_color
+                    and visited[r + dr][c + dc] == 0
                 ):
-                    dfs(image, sr + dr, sc + dc, color)
+                    dfs(image, r + dr, c + dc, color)
 
         dfs(image, sr, sc, color)
         return image
