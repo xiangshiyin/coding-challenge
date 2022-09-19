04495eba6d024648f0e65ac6c23dc081a0633857
Xiangshi Yin <piupiuyin@gmail.com>
2022-09-18 22:28:06 -0400

    2 dp

--- lc-2022/62.unique-paths.py
+++ lc-2022/62.unique-paths.py
@@ -5,12 +5,31 @@
 #
 
 # @lc code=start
+# class Solution:
+#     def uniquePaths(self, m: int, n: int) -> int:
+#         grid = [[0] * n for i in range(m)]
+#         for i in range(m):
+#             for j in range(n):
+#                 if i == 0 or (i == 1 and j == 0):  # first row
+#                     grid[i][j] = 1
+#                 else:
+#                     grid[i][j] = grid[i][j - 1] + grid[i - 1][j]
+#         return grid[m - 1][n - 1]
+
+
+# solution on 2022-09-18
+from re import L
+
+
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        """
+        grid[i][j] = grid[i][j-1] + grid[i-1][j]
+        """
         grid = [[0] * n for i in range(m)]
         for i in range(m):
             for j in range(n):
-                if i == 0 or (i == 1 and j == 0):  # first row
+                if i == 0 or j == 0:
                     grid[i][j] = 1
                 else:
                     grid[i][j] = grid[i][j - 1] + grid[i - 1][j]
--- lc-2022/746.min-cost-climbing-stairs.py
+++ lc-2022/746.min-cost-climbing-stairs.py
@@ -5,16 +5,39 @@
 #
 
 # @lc code=start
+# class Solution:
+#     def minCostClimbingStairs(self, cost: List[int]) -> int:
+#         n = len(cost)
+#         grid = [0] * n
+#         for i in range(n):
+#             if i <= 1:
+#                 grid[i] = 0
+#             else:
+#                 grid[i] = min(grid[i - 2] + cost[i - 2], grid[i - 1] + cost[i - 1])
+#         return min(grid[n - 2] + cost[n - 2], grid[n - 1] + cost[n - 1])
+
+
+# solution on 2022-09-18
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        grid = [0] * n
-        for i in range(n):
-            if i <= 1:
-                grid[i] = 0
+        """
+        grid[i] = min(grid[i-1] + cost[i-1], grid[i-2] + cost[i-2])
+        """
+        from functools import lru_cache
+
+        @lru_cache
+        def grid(i):
+            """
+            minimum cost to reach step i
+            """
+            if i == 0:
+                return 0
+            elif i == 1:
+                return min(cost[0], 0)
             else:
-                grid[i] = min(grid[i - 2] + cost[i - 2], grid[i - 1] + cost[i - 1])
-        return min(grid[n - 2] + cost[n - 2], grid[n - 1] + cost[n - 1])
+                return min(grid(i - 1) + cost[i - 1], grid(i - 2) + cost[i - 2])
+
+        return grid(len(cost))
 
 
 # @lc code=end
