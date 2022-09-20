61cde972faf3bf109ed706fb3468541dccc42dbd
Xiangshi Yin <piupiuyin@gmail.com>
2022-09-19 23:49:50 -0400

    2 more

--- lc-2022/424.longest-repeating-character-replacement.py
+++ lc-2022/424.longest-repeating-character-replacement.py
@@ -5,26 +5,51 @@
 #
 
 # @lc code=start
+# class Solution:
+#     def characterReplacement(self, s: str, k: int) -> int:
+#         from collections import defaultdict
+
+#         n = len(s)
+#         l = 0
+#         r = 0
+#         max_L = 0
+#         seen = defaultdict(int)
+#         maxCount = 0
+#         while l <= r and r < n:
+#             seen[s[r]] += 1
+#             maxCount = max(maxCount, seen[s[r]])
+#             while r - l + 1 - maxCount > k:
+#                 seen[s[l]] -= 1
+#                 l += 1
+#             max_L = max(max_L, r - l + 1)
+#             r += 1
+
+#         return max_L
+
+
+# solution on 2022-09-19
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        from collections import defaultdict
-
+        """
+        two pointer, moving window
+        """
         n = len(s)
         l = 0
         r = 0
-        max_L = 0
-        seen = defaultdict(int)
         maxCount = 0
-        while l <= r and r < n:
-            seen[s[r]] += 1
-            maxCount = max(maxCount, seen[s[r]])
-            while r - l + 1 - maxCount > k:
-                seen[s[l]] -= 1
-                l += 1
-            max_L = max(max_L, r - l + 1)
-            r += 1
+        maxL = 0
+        from collections import defaultdict
 
-        return max_L
+        freq = defaultdict(int)
+        while l <= r and r < n:
+            freq[s[r]] += 1
+            maxCount = max(maxCount, freq[s[r]])
+            while r - l + 1 - maxCount > k:
+                freq[s[l]] -= 1
+                l += 1
+            maxL = max(maxL, r - l + 1)
+            r += 1
+        return maxL
 
 
 # @lc code=end
--- lc-2022/438.find-all-anagrams-in-a-string.py
+++ lc-2022/438.find-all-anagrams-in-a-string.py
@@ -5,32 +5,66 @@
 #
 
 # @lc code=start
+# class Solution:
+#     def findAnagrams(self, s: str, p: str) -> List[int]:
+#         from collections import Counter, defaultdict
+
+#         pattern = Counter(p)
+#         ans = []
+#         found = defaultdict(int)
+#         start = -1
+#         for i in range(len(s)):
+#             if s[i] in pattern:
+#                 if start == -1:
+#                     start = i
+
+#                 found[s[i]] += 1
+#                 if i - start + 1 == len(p):
+#                     if found == pattern:
+#                         ans.append(start)
+#                     found[s[start]] -= 1
+#                     start += 1
+#             else:
+#                 if found:
+#                     if found == pattern:
+#                         ans.append(start)
+#                     else:
+#                         found = defaultdict(int)
+#                     start = -1
+#         return ans
+
+
+# solution on 2022-09-19
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        from collections import Counter, defaultdict
+        from collections import defaultdict, Counter
 
-        pattern = Counter(p)
+        freq = Counter(p)
+        sofar = defaultdict(int)
+        n = len(s)
+        l = 0
+        r = 0
         ans = []
-        found = defaultdict(int)
-        start = -1
-        for i in range(len(s)):
-            if s[i] in pattern:
-                if start == -1:
-                    start = i
-
-                found[s[i]] += 1
-                if i - start + 1 == len(p):
-                    if found == pattern:
-                        ans.append(start)
-                    found[s[start]] -= 1
-                    start += 1
+        while l <= r and r < n:
+            if s[r] not in freq:
+                r += 1
+                l = r
             else:
-                if found:
-                    if found == pattern:
-                        ans.append(start)
+                if l == r:
+                    sofar = defaultdict(int)
+                sofar[s[r]] += 1
+
+                while r - l + 1 > len(p):
+                    sofar[s[l]] -= 1
+                    l += 1
+
+                if r - l + 1 == len(p):
+                    if sofar == freq:
+                        ans.append(l)
                     else:
-                        found = defaultdict(int)
-                    start = -1
+                        sofar[s[l]] -= 1
+                        l += 1
+                r += 1
         return ans
 
 
