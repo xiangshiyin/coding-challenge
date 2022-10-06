66facdd0e978b652faca888db4cb95b907ae1f40
Xiangshi Yin <piupiuyin@gmail.com>
2022-10-05 23:23:37 -0400

    bucket sort

--- lc-2022/347.top-k-frequent-elements.py
+++ lc-2022/347.top-k-frequent-elements.py
@@ -5,14 +5,39 @@
 #
 
 # @lc code=start
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#         import heapq
+#         from collections import Counter
+
+#         freq = Counter(nums)
+
+#         return heapq.nlargest(k, freq.keys(), lambda key: freq[key])
+
+
+# solution on 2022-10-05
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        import heapq
-        from collections import Counter
+        from collections import Counter, defaultdict
 
         freq = Counter(nums)
+        buckets = defaultdict(list)
+        for v, f in freq.items():
+            buckets[f].append(v)
 
-        return heapq.nlargest(k, freq.keys(), lambda key: freq[key])
+        n = len(nums)
+        bucketIndex = n
+        ans = []
+        while k > 0:
+            while not buckets[bucketIndex]:
+                bucketIndex -= 1
+            for num in buckets[bucketIndex]:
+                if k == 0:
+                    break
+                ans.append(num)
+                k -= 1
+            bucketIndex -= 1
+        return ans
 
 
 # @lc code=end
