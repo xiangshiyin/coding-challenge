c11c5941a994b77122e3ebff431bc058081a8b86
Xiangshi Yin <piupiuyin@gmail.com>
2025-11-11 22:56:54 -0500

    1st trial in 2025

---
+++ lc/lc-2025/102.Binary Tree Level Order Traversal.py
@@ -0,0 +1,57 @@
+#
+# @lc app=leetcode id=102 lang=python3
+# @lcpr version=30304
+#
+# [102] Binary Tree Level Order Traversal
+#
+
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+from typing import List, Optional
+from collections import deque
+
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        q = deque([root])
+        res = []
+        while q:
+            tmp = []
+            for i in range(len(q)):
+                cur = q.popleft()
+                tmp.append(cur.val)
+                if cur.left:
+                    q.append(cur.left)
+                if cur.right:
+                    q.append(cur.right)
+            res.append(tmp)
+        return res
+
+
+# @lc code=end
+
+
+#
+# @lcpr case=start
+# [3,9,20,null,null,15,7]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [1]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# []\n
+# @lcpr case=end
+
+#
