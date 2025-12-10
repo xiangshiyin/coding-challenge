6dcdf4cd3a37942db3570c7e59470f97a749d929
Xiangshi Yin <piupiuyin@gmail.com>
2025-12-10 14:40:40 -0500

    new code

---
+++ lc/lc-2025/590. N-ary Tree Postorder Traversal.py
@@ -0,0 +1,9 @@
+class Solution:
+    def postorder(self, root: "Node") -> List[int]:
+        if not root:
+            return []
+        output = []
+        for c in root.children:
+            output += self.postorder(c)
+        output.append(root.val)
+        return output
